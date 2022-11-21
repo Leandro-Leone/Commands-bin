15254baad647fec591d0b91a19656aa6f3c09626
Leandro-Leone <84367018+Leandro-Leone@users.noreply.github.com>
2022-11-20 23:32:30 -0300

    Add files via upload

---
+++ zsh_install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+
+echo \
+"
+	1) Install zsh [from curl]
+	2) Install zsh-autosuggestionszsh
+	3) Install zsh-syntax-highlighting
+	4) Download powerlevel10k
+"
+
+read -p "Choose one option: " main_option
+
+
+
+case "$main_option" in
+
+	1)
+
+		{ sh -c \"$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh); }
+
+	;;
+
+	2)
+		{ cd; cd .oh-my-zsh/plugins/; git clone https://github.com/zsh-users/zsh-autosuggestions; }
+
+	;;
+
+	3)
+
+		{ cd; cd .oh-my-zsh/plugins/; git clone https://github.com/zsh-users/zsh-syntax-highlighting; }
+	;;
+
+	4)
+
+		{ cd; git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k}
+
+	;;
