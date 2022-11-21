240a9cfdcc08b13441bb3ce4269af178b1975c7d
Leandro-Leone <84367018+Leandro-Leone@users.noreply.github.com>
2022-11-20 23:33:53 -0300

    Update zsh_install.sh

--- zsh_install.sh
+++ zsh_install.sh
@@ -36,3 +36,9 @@ case "$main_option" in
 		{ cd; git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k}
 
 	;;
+	
+	
+	*)
+	
+		echo "No such option $main_option"; exit 1
+esac
