581c33da2d70697baee5b7a51a369e1595d215d7
Leandro-Leone <84367018+Leandro-Leone@users.noreply.github.com>
2022-11-26 13:01:13 -0300

    Create  download-yt.sh

---
+++ download-yt/download-yt.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+clear
+
+
+# Variables
+
+quality="320k"
+format="mp3"
+
+
+source help.txt.conf
+
+# youtube-dl --embed-thumbnail --audio-quality "$quality" --audio-format "$format_audio"
+
+case "$1" in
+
+	-h|--) echo $help;;
+
+	-d|--download) youtube-dl -x --embed-thumbnail --audio-format mp3 "$2" ;;
+
+	*) { echo "Type $0.sh --help for more information"; exit 1; }
+
+	;;
+
+esac
